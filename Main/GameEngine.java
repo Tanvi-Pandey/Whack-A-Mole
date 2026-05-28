c2e02be9f600e8a2dfa15c3b4a27345157c88ec6
Tanvi-Pandey <tanvip0709@gmail.com>
2026-05-28 12:50:11 +0530

    Added game engine and multithreading

--- Main/GameEngine.java
+++ Main/GameEngine.java
@@ -1,3 +1,40 @@
-public class GameEngine {
-    
+package main;
+
+import javax.swing.*;
+
+public class GameEngine implements Runnable {
+
+    private GamePanel panel;
+
+    private boolean running = true;
+
+    public GameEngine(GamePanel panel) {
+        this.panel = panel;
+    }
+
+    @Override
+    public void run() {
+
+        while(running) {
+
+            try {
+
+                SwingUtilities.invokeLater(() -> {
+
+                    panel.spawnRandomOccupant();
+
+                    panel.updateTimer();
+                });
+
+                Thread.sleep(1000);
+
+            }
+            catch (InterruptedException e) {
+
+                running = false;
+
+                System.out.println("Game thread stopped");
+            }
+        }
+    }
 }
